f3de804b7f5f2155ef03a3979af187a1d14567df
Michal <Michal@gmail.com>
2020-02-16 15:01:00 +0100

    Add discovery client

--- src/main/java/com/testing/TestingApplication.java
+++ src/main/java/com/testing/TestingApplication.java
@@ -2,8 +2,10 @@ package com.testing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TestingApplication {
 
 	public static void main(String[] args) {
